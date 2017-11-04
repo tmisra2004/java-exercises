bbe7d1764a11f3405e859eb8e968cd695220d280
Tavish Misra <tmisra2004@gmail.com>
2017-11-04 15:55:05 -0500

    Completed first studio with Bonus missions achieved.

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -7,20 +7,26 @@ public class Area {
     public static final double pi = 3.14;
 
     public static void main(String[] args) {
-        System.out.print("Enter a radius: ");
+
         Scanner input = new Scanner(System.in);
-        double radius = input.nextDouble();
-        getArea(radius);
-        if (radius < 0) {
-            System.out.println("The radius cannot be negative.");
+        System.out.print("Enter a radius: ");
+        double r = input.nextDouble();
+        double area = pi * r * r;
+        while (r < 0)
+        {
+            System.out.println("Radius cannot be negative.");
             System.out.println();
+            System.out.print("Enter a radius: ");
+            r = input.nextDouble();
+
         }
 
+        System.out.println("The area of a circle of radius " + r + " units is " + area + " square units.");
+
+
     }
 
-    public static String getArea(double r)
-    {
-        double area = pi * r * r;
-        return "The area of a circle of radius " + r + " units is " + area + " square units.";
-    }
+
+
+
 }
