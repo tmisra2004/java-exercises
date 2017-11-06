342c6ab019b7904e17d37240b8182b790c0cdd4d
Tavish Misra <tmisra2004@gmail.com>
2017-11-06 15:19:21 -0600

    Finished third of 4 exercises for class 3.2 prep.

---
+++ src/exercises/FibonacciArray.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class FibonacciArray {
+
+    public static void main(String[] args)
+    {
+        int[] fbNumArray = {1, 1, 2, 3, 5, 8};
+        for (int i = 0; i < fbNumArray.length; i++)
+        {
+            System.out.println(fbNumArray[i]);
+        }
+    }
+}
